a9dda9554340dfb88db1cf6e8b46b1ba17315ca4
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-11-24 10:47:44 +0600

    branding part totally done

--- src/pages/Homepage/Hero/Hero.js
+++ src/pages/Homepage/Hero/Hero.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import macbook from '../../../assets/image/macbook.png'
 
 const Hero = () => {
     return (
         <div>
-            <section className="bg-white dark:bg-gray-900 h-[90vh]">
+            <section className="bg-white dark:bg-gray-900 h-[90vh] flex items-center">
     <div className="grid max-w-screen-xl  px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Resell Old Waste your Money</h1>
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white mb-6">Resell Old Waste your Money</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
             <Link href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-[#fff] rounded-lg bg-primary hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Get started
@@ -18,7 +19,7 @@ const Hero = () => {
             </Link> 
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"/>
+            <img src={macbook} alt="mockup"/>
         </div>                
     </div>
 </section>
