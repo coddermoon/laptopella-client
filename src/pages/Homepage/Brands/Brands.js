940803a79529bfe5dfae52ae8829d9c095c2a828
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-11-24 10:40:24 +0600

    branding added

---
+++ src/pages/Homepage/Brands/Brands.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import  { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import brand1 from '../../../assets/image/brands/brand-1.png'
+import brand2 from '../../../assets/image/brands/brand-2.png'
+import brand3 from '../../../assets/image/brands/brand-3.png'
+import brand4 from '../../../assets/image/brands/brand-4.png'
+import "swiper/css/bundle";
+const Brands = () => {
+
+    const brandsPhoto = [brand1,brand2,brand3,brand4]
+
+    return (
+        <div div className='container mx-auto'>
+            <div>
+                <h2 className='text-center text-4xl font-bold my-5'>Our Brands</h2>
+                <p className='w-2/3 mx-auto  mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia voluptatum laboriosam eum veritatis magnam eius exercitationem eligendi consequuntur nemo quidem!</p>
+            </div>
+        <Swiper
+          slidesPerView={5}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide className='bg-[#fff]'>
+
+<img className='' src={brand1} alt="" />
+
+          </SwiperSlide>
+
+          {
+            brandsPhoto.map(photo=> <SwiperSlide className='bg-[#fff]'>
+
+            <img src={photo} alt="" />
+            
+                      </SwiperSlide>)
+          }
+          
+        </Swiper>
+      </div>
+    );
+};
+
+export default Brands;
--- src/pages/Homepage/Home/Home.js
+++ src/pages/Homepage/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Brands from '../Brands/Brands';
 import Hero from '../Hero/Hero';
 
 const Home = () => {
     return (
         <main >
             <Hero/>
+            <Brands/>
         </main>
     );
 };
