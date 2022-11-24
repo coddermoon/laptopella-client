01f63fbc5223deb02ad50e4c79a5f79dddae0a31
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-11-24 09:40:41 +0600

    hero section added done

--- src/pages/Homepage/Home/Home.js
+++ src/pages/Homepage/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Hero from '../Hero/Hero';
 
 const Home = () => {
     return (
-        <div >
-            this is homepage
-        </div>
+        <main >
+            <Hero/>
+        </main>
     );
 };
 
