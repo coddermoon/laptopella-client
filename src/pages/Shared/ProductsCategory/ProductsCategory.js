75b36c4c7113c4c194539d8042551c9a84a5266f
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-11-25 11:56:02 +0600

    done load categories

--- src/pages/Shared/ProductsCategory/ProductsCategory.js
+++ src/pages/Shared/ProductsCategory/ProductsCategory.js
@@ -1,15 +1,30 @@
 
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const ProductsCategory = () => {
+    const [categoty,setCategory]= useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:5000/category')
+        .then(res=>setCategory(res.data))
+    },[])
+    console.log(categoty)
     return (
         
 <div className='text-center'>
 <div className="inline-flex rounded-md shadow-sm text-center" role="group">
+    {
+        categoty.map((item, index)=> <button
+        key={index}
+        className='bg-primary px-3 py-1 text-[#fff] m-2 shadow-lg'>{item}</button>
+            
+       )
+    }
  
-<button className='bg-primary px-3 py-1 text-[#fff] m-2 shadow-lg'>Dell</button>
 
-<button className='bg-[#fff] px-3 py-1 text-primary border-primary m-2 shadow-lg'>Dell</button>
+
+
  
  
 </div>
