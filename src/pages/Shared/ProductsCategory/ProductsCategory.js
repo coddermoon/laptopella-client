24103ef9e1987a7ad36392607cc1e6d3171c563d
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-11-25 12:02:25 +0600

    category loaded

--- src/pages/Shared/ProductsCategory/ProductsCategory.js
+++ src/pages/Shared/ProductsCategory/ProductsCategory.js
@@ -9,11 +9,14 @@ const ProductsCategory = () => {
         axios.get('http://localhost:5000/category')
         .then(res=>setCategory(res.data))
     },[])
-    console.log(categoty)
+  
     return (
         
 <div className='text-center'>
 <div className="inline-flex rounded-md shadow-sm text-center" role="group">
+
+    <button className='bg-primary px-3 py-1 text-[#fff] m-2 shadow-lg'>All</button>
+    
     {
         categoty.map((item, index)=> <button
         key={index}
