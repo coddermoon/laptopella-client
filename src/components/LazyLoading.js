cacc3776ef29a1763f5aadf1c19a5d637648bdb0
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-11-25 14:10:28 +0600

    updated

--- src/components/LazyLoading.js
+++ src/components/LazyLoading.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 const LazyLoading = () => {
     return (
-        <div className="bg-gray-300 rounded-md shadow-md px-8 py-8 flex space-x-4 cursor-pointer items-center flex-col text-center space-y-4 lg:flex-row lg:text-left lg:space-y-0 animate-pulse">
-        <div className="ring rounded-full h-24 w-24 lg:h-16 lg:w-16 bg-gray-300 animate-pulse"></div>
-        <div>
-          <div className="font-regular text-xl mb-2 lg:w-60 h-4 bg-gray-400 animate-pulse"></div>
-          <div className="font-regular text-xl mb-2 lg:w-24 h-4 bg-gray-400 animate-pulse"></div>
-          <div className="font-regular text-xl mb-2 lg:w-36 h-4 bg-gray-400 animate-pulse"></div>
-          <p className="underline text-blue-500 font-medium"></p>
-          <p className="font-medium"></p>
+       
+<div class="w-full h-screen border-2 rounded-md mx-auto mt-20">
+  <div class="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
+    <div class="w-12 bg-gray-300 h-12 rounded-full ">
+    </div>
+        <div class="flex flex-col space-y-3">
+        <div class="w-36 bg-gray-300 h-6 rounded-md ">
         </div>
-      </div>
+        <div class="w-24 bg-gray-300 h-6 rounded-md ">
+        </div>
+    </div>
+  </div>
+</div>
+
     );
 };
 
--- src/pages/Homepage/Brands/Brands.js
+++ src/pages/Homepage/Brands/Brands.js
@@ -1,56 +1,50 @@
-import React from 'react';
-import  { Pagination } from 'swiper';
+import React from "react";
+import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import brand1 from '../../../assets/image/brands/brand-1.png'
-import brand2 from '../../../assets/image/brands/brand-2.png'
-import brand3 from '../../../assets/image/brands/brand-3.png'
-import brand4 from '../../../assets/image/brands/brand-4.png'
+import brand1 from "../../../assets/image/brands/brand-1.png";
+import brand2 from "../../../assets/image/brands/brand-2.png";
+import brand3 from "../../../assets/image/brands/brand-3.png";
+import brand4 from "../../../assets/image/brands/brand-4.png";
 import "swiper/css/bundle";
 const Brands = () => {
+  const brandsPhoto = [
+    { id: 1, logo: brand1 },
+    { id: 2, logo: brand2 },
+    { id: 3, logo: brand3 },
+    { id: 4, logo: brand4 },
+    { id: 5, logo: brand2 },
+    { id: 6, logo: brand4 },
+    { id: 7, logo: brand1 },
+  ];
 
-    const brandsPhoto = [
-      {id:1, logo:brand1},
-      {id:2, logo:brand2},
-      {id:3, logo:brand3},
-      {id:4, logo:brand4},
-      {id:5, logo:brand2},
-      {id:6, logo:brand4},
-      {id:7, logo:brand1},
-    ]
-
-    return (
-        <div div className='container mx-auto my-5'>
-            <div className='my-10'>
-                <h2 className='text-center text-4xl font-bold my-5'>Our Brands</h2>
-                <p className='w-2/3 mx-auto  mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia voluptatum laboriosam eum veritatis magnam eius exercitationem eligendi consequuntur nemo quidem!</p>
-            </div>
-        <Swiper
-          slidesPerView={8}
-          spaceBetween={30}
-         
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide className='bg-[#fff]'>
-
-<img className='' src={brand1} alt="" />
-
-          </SwiperSlide>
-
-          {
-            brandsPhoto.map(photo=> <SwiperSlide
-            
-            key={photo.id}
-            className='bg-[#fff]'>
-
-            <img src={photo} alt="" />
-            
-                      </SwiperSlide>)
-          }
-          
-        </Swiper>
+  return (
+    <div div className="container mx-auto my-5">
+      <div className="my-10">
+        <h2 className="text-center text-4xl font-bold my-5">Our Brands</h2>
+        <p className="w-2/3 mx-auto  mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+          voluptatum laboriosam eum veritatis magnam eius exercitationem
+          eligendi consequuntur nemo quidem!
+        </p>
       </div>
-    );
+      <Swiper
+        slidesPerView={8}
+        spaceBetween={30}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="bg-[#fff]">
+          <img className="" src={brand1} alt="" />
+        </SwiperSlide>
+
+        {brandsPhoto.map((photo) => (
+          <SwiperSlide key={photo.id} className="bg-[#fff]">
+            <img src={photo.logo} alt="hello" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Brands;
